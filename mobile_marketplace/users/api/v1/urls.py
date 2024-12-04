58ffe686e584fb79e5112d5f005c8d97db441259
muhammadaounzaidi <zaoun123@gmail.com>
2024-12-04 13:26:40 +0500

    Added Consistent Quotation Marks in users urls

--- mobile_marketplace/users/api/v1/urls.py
+++ mobile_marketplace/users/api/v1/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from mobile_marketplace.users.api.v1.views import RegisterView
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("register/", RegisterView.as_view(), name="register"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
