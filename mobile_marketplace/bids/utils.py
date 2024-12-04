ca1f750dac1da78441e13d95d55cf0948b29af70
muhammadaounzaidi <zaoun123@gmail.com>
2024-12-04 13:29:58 +0500

    changed has_valid_function

--- mobile_marketplace/bids/utils.py
+++ mobile_marketplace/bids/utils.py
@@ -1,6 +1,2 @@
-from mobile_marketplace.bids.models import Bid
-
-
 def has_valid_bid(self):
-    bid = Bid.objects.filter(mobile=self.mobile).last()
-    return bid is not None and bid.amount > 0
+    return self.amount > 0
